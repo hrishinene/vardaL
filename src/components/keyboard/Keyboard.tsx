00eeb1828fa3052460dd34f925d7e4adee04ce40
Hrishikesh Nene <hrishi@mycariq.com>
2022-03-02 18:08:17 +0530

    Keyboard layout change and all words in

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -45,67 +45,55 @@ export const Keyboard = ({onChar, onDelete, onEnter, guesses}: Props) => {
     }, [onEnter, onDelete, onChar])
 
     return (
-        <div>
-            <div className="flex justify-center mb-1">
-                <div>
-                <Key width={50} value="ENTER" onClick={onClick}>
-                    {ENTER_TEXT}
-                </Key>
-            </div>
-                <div>
+<div>
+<div className="flex justify-center mb-1">
 <Key value={KeyVal(charStatuses, "क").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'क').status}/>
 <Key value={KeyVal(charStatuses, "ख").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'ख').status}/>
 <Key value={KeyVal(charStatuses, "ग").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'ग').status}/>
 <Key value={KeyVal(charStatuses, "घ").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'घ').status}/>
-<Key value={KeyVal(charStatuses, "ङ").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'ङ').status}/>
+<Key width={50} value="ENTER" onClick={onClick}> {ENTER_TEXT} </Key>
 </div>
- <div>
+<div className="flex justify-center mb-1">
 <Key value={KeyVal(charStatuses, "च").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'च').status}/>
 <Key value={KeyVal(charStatuses, "छ").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'छ').status}/>
 <Key value={KeyVal(charStatuses, "ज").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'ज').status}/>
 <Key value={KeyVal(charStatuses, "झ").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'झ').status}/>
-<Key value={KeyVal(charStatuses, "ञ").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'ञ').status}/>
+<Key width={50} value="DELETE" onClick={onClick}> {DELETE_TEXT} </Key>
 </div>
- <div>
+<div className="flex justify-center mb-1">
 <Key value={KeyVal(charStatuses, "ट").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'ट').status}/>
 <Key value={KeyVal(charStatuses, "ठ").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'ठ').status}/>
 <Key value={KeyVal(charStatuses, "ड").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'ड').status}/>
 <Key value={KeyVal(charStatuses, "ढ").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'ढ').status}/>
 <Key value={KeyVal(charStatuses, "ण").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'ण').status}/>
 </div>
- <div>
+<div className="flex justify-center mb-1">
 <Key value={KeyVal(charStatuses, "त").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'त').status}/>
 <Key value={KeyVal(charStatuses, "थ").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'थ').status}/>
 <Key value={KeyVal(charStatuses, "द").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'द').status}/>
 <Key value={KeyVal(charStatuses, "ध").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'ध').status}/>
 <Key value={KeyVal(charStatuses, "न").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'न').status}/>
 </div>
- <div>
+<div className="flex justify-center mb-1">
 <Key value={KeyVal(charStatuses, "प").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'प').status}/>
 <Key value={KeyVal(charStatuses, "फ").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'फ').status}/>
 <Key value={KeyVal(charStatuses, "ब").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'ब').status}/>
 <Key value={KeyVal(charStatuses, "भ").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'भ').status}/>
 <Key value={KeyVal(charStatuses, "म").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'म').status}/>
 </div>
- <div>
+<div className="flex justify-center mb-1">
 <Key value={KeyVal(charStatuses, "य").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'य').status}/>
 <Key value={KeyVal(charStatuses, "र").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'र').status}/>
 <Key value={KeyVal(charStatuses, "ल").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'ल').status}/>
 <Key value={KeyVal(charStatuses, "व").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'व').status}/>
 <Key value={KeyVal(charStatuses, "श").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'श').status}/>
 </div>
- <div>
+<div className="flex justify-center mb-1">
 <Key value={KeyVal(charStatuses, "ष").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'ष').status}/>
 <Key value={KeyVal(charStatuses, "स").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'स').status}/>
 <Key value={KeyVal(charStatuses, "ह").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'ह').status}/>
 <Key value={KeyVal(charStatuses, "ळ").chrForm.chrForm} onClick={onClick} status={KeyVal(charStatuses, 'ळ').status}/>
 </div>
- <div>
-<Key width={50} value="DELETE" onClick={onClick}>
-{DELETE_TEXT}
-</Key>
 </div>
-            </div>
-        </div>
     )
 }
