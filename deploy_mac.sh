1e1a015fbb28b971413d8bd47d99c271bf8b787f
hrishinene <hrishikesh.nene@gmail.com>
2022-08-27 13:19:25 +0530

    Deployment to mac

---
+++ deploy_mac.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+PWD=`pwd`
+D=/tmp/deploy$$;
+echo "Temp Dir: $D"
+mkdir $D
+cd $D
+echo "Setting projectid to shabdak2"
+gcloud config set project shabdak2
+
+echo "Cloning repository to directory: $D"
+git clone git@github.com:hrishinene/vardaL.git
+cd vardaL
+git fetch
+git checkout shabdak_2
+git pull
+
+echo "Running npm build"
+npm i
+npm run build
+# rm -rf -v !("build"|"app.yaml")
+rm -rf `ls |grep -v "build\|app.yaml"`
+
+echo "Deploying to Google Cloud"
+gcloud app deploy --quiet
+# gcloud app deploy
+
+echo "Deployment Done! Cleaning up!"
+cd /tmp
+rm -rf $D
+cd $PWD
+
